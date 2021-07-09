5e389881612bdba06b08e2c084a4ba8960bd814c
Joshua Byrd <phocks@gmail.com>
2021-07-09 14:21:43 +1000

    Quick new version use Sydney

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -7,7 +7,12 @@
   const km = parseInt(decodeURIComponent(kmString), 10);
   const radius: number = (isNaN(km) ? 5 : km) * 1000;
 
-  let location: LatLon = [-37.7975037, 144.9603923];
+  // Melbourne latlong
+  // let location: LatLon = [-37.7975037, 144.9603923];
+
+  // Sydney latlong
+  // TODO: Make this configurable in URL
+  let location: LatLon = [-33.86748, 151.20727];
 </script>
 
 <Map {location} {radius} />
